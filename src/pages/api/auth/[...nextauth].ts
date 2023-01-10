45c75cf6bcc564649563831c8daaf0982838516c
Rudinei Costa <costaaugusto177@outlook.com>
2023-01-10 15:25:35 -0300

    feat: create- condicional query para cadastro e logins

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import {fauna} from '../../../services/fauna'
+import {query as q} from 'faunadb'
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -7,11 +9,44 @@ export const authOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-      // scope:'read:user'
-      
     }),
-    // ...add more providers here
   ],
+  // jwt:{
+  //   signingKey: process.env.JWT_SIGNKEY,
+  // },
+  callbacks: {
+    async signIn({ user, account, profile, credentials }) {
+      const {email} = user
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              {data: {email}}
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+        return true
+      }catch{
+        return false
+      } 
+    },
+  }
 }
 
 export default NextAuth(authOptions)
--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,8 +1,7 @@
 import Stripe from 'stripe';
 import {version} from '../../package.json';
-
 export const stripe = new Stripe(
-  process.env.STRIPE_API_KEY,
+  process.env.STRIPE_SK_KEY,
   {
     apiVersion: '2022-11-15',
     appInfo:{
